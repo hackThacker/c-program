7d5d17369060241c314c617d3e8369ea62172d09
hackThacker <nirajadhiakri572@gmail.com>
2025-05-10 16:23:19 +0545

    completed constants

---
+++ c basics/6. Constants.c
@@ -0,0 +1,15 @@
+// created by HackTHacker
+#include <stdio.h>
+
+int main() {
+    const int minutesPerHour = 60;
+
+    // Printing the value of the constant
+    printf("%d\n", minutesPerHour);
+
+    const int BIRTHYEAR = 1980;
+
+  printf("%d", BIRTHYEAR);
+
+    return 0;
+}
